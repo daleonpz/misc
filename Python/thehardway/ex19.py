4418908214f9ea5e8b34d8388990a9270c3fe310
Daniel Paredes <daleonpz@gmail.com>
2016-06-08 16:44:20 -0500

    ex15 - ex21

---
+++ Python/thehardway/ex19.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python2.7
+
+from sys import argv
+
+def cheese_and_crackers( cheese_count, boxes_of_crackers ):
+	print "You have %d cheeses!" % cheese_count
+	print "You have %d boxes of crackers!" % boxes_of_crackers
+	print "Man that's enough for a party!"
+	print "Get a blanket.\n"
+
+print "We can just give the function numbers directly:"
+cheese_and_crackers( 20, 30)
+
+print "OR, we can use variables from our script:"
+amount_of_cheese = 10
+amount_of_crackers = 50
+
+cheese_and_crackers( amount_of_cheese, amount_of_crackers )
+
+print "We can even do math inside too:"
+cheese_and_crackers( 10 + 20, 5 + 6)
+
+print "And we can combine the two, variables and math:"
+cheese_and_crackers( amount_of_cheese + 100, amount_of_crackers + 1000)
+
+
+# STUDY DRILL
+
+def print_something( something ):
+	print "%r" % something
+
+print_something( "Hola" )
+print_something( str(2) + "hola")
+
+script, something = argv
+
+print_something( something )
+
+something = raw_input(">")
+
+print_something( something )
