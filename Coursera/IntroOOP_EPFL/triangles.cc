7e561f8c1cfbfb09352c11d58bc2bd1743826c21
Daniel Paredes <daleonpz@gmail.com>
2016-08-02 13:56:51 -0500

    improved triangle.cc

--- Coursera/IntroOOP_EPFL/triangles.cc
+++ Coursera/IntroOOP_EPFL/triangles.cc
@@ -28,31 +28,28 @@ struct TriangleSides{
 	double x, y, z;
 };
 
+double distance(struct Point3D *p1, struct Point3D *p2){
+		return sqrtf(
+				  (p1->x - p2->x)*(p1->x - p2->x) 
+				+ (p1->y - p2->y)*(p1->y - p2->y)
+				+ (p1->z - p2->z)*(p1->z - p2->z) 
+				);
+}
+
 class Triangle {
 	private:
 		TriangleSides sides_;
 	public:
+		void init();
 		void setSides(struct Point3D *p1, struct Point3D *p2, struct Point3D *p3);
 		bool isIsocele() const;
 		double getPerimetre() const;
 };
 
 	void Triangle::setSides(struct Point3D *p1, struct Point3D *p2, struct Point3D *p3){
-		sides_.x = (p1->x - p2->x)*(p1->x - p2->x) 
-				+ (p1->y - p2->y)*(p1->y - p2->y)
-				+ (p1->z - p2->z)*(p1->z - p2->z) ;
-				
-		sides_.y = (p1->x - p3->x)*(p1->x - p3->x) 
-				+ (p1->y - p3->y)*(p1->y - p3->y)
-				+ (p1->z - p3->z)*(p1->z - p3->z) ;
-		
-		sides_.z = (p3->x - p2->x)*(p3->x - p2->x) 
-				+ (p3->y - p2->y)*(p3->y - p2->y)
-				+ (p3->z - p2->z)*(p3->z - p2->z) ;
-				
-		sides_.x = sqrtf(sides_.x);
-		sides_.y = sqrtf(sides_.y);
-		sides_.z = sqrtf(sides_.z);
+		sides_.x = distance(p1, p2);
+		sides_.y = distance(p2, p3);
+		sides_.z = distance(p1, p3);
 	}
 	
 	bool Triangle::isIsocele() const{
@@ -65,19 +62,23 @@ class Triangle {
 		return (sides_.x + sides_.y + sides_.z);
 	}
 
+	void Triangle::init() {
+		Point3D p1,p2,p3;
+		create_newPoint(&p1);
+		create_newPoint(&p2);
+		create_newPoint(&p3);
+		
+		this->setSides(&p1, &p2, &p3);
+	}
+
+
 int main (int argc, char *argv[]){
 	
 	double x, y, z;
 	
-	Point3D p1,p2,p3;
-	
 	Triangle tri;
 	
-	create_newPoint(&p1);
-	create_newPoint(&p2);
-	create_newPoint(&p3);
-	
-	tri.setSides(&p1, &p2, &p3);
+	tri.init();
 	
 	cout << "Périmètre: " << setprecision(32) << tri.getPerimetre() << endl;
 	
