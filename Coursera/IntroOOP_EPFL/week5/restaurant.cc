bf7da5c3e36c52da926720464cefc7b70986e46b
Daniel Paredes <daleonpz@gmail.com>
2016-09-30 17:09:18 -0500

    working on restaurant.cc

--- Coursera/IntroOOP_EPFL/week5/restaurant.cc
+++ Coursera/IntroOOP_EPFL/week5/restaurant.cc
@@ -7,6 +7,147 @@ using namespace std;
 /*****************************************************
   * Compléter le code à partir d'ici
  *****************************************************/
+class Produit{
+	private: 
+		string nom_;
+	        string unite;
+	
+	public:
+		Produit(string n, string u = "")
+			:nom_(n), unite(u){}
+
+		string getNom() const{ return nom_;  }
+		string getUnite() const{ return unite; }
+
+		virtual string toString( ) const{ 
+			return  getNom(); 
+		}
+		
+		virtual const Produit* adapter( double ) const{
+			Produit* p = new Produit( nom_, unite  );
+			return p;
+		}
+
+		virtual double quantiteTotale( const string& nomProduit  ) const{
+			return nomProduit==getNom()?1:0;
+		}
+};
+
+class Ingredient{
+	private:
+		Produit* produit_;
+		double quantity_;
+	
+	public:
+		Ingredient(const Produit& p, double q )
+			:produit_(new Produit(p.getNom(), p.getUnite())),quantity_(q){}
+
+		const Produit& getProduit() const{ return *produit_;  }
+		double getQuantity() const{ return quantity_; }
+
+		string descriptionAdaptee( ) {
+			std::ostringstream os;
+
+			os<< getQuantity();
+			return os.str() + " " + getProduit().getUnite() 
+				+ " de " + (getProduit().adapter( getQuantity()))->toString() ;
+		//	return  os.str() + " " + getProduit().getUnite() 
+		//		+ " de " + getProduit().getNom()  ;
+		}
+
+		string toString( ) const {
+			return getProduit().toString();
+		}
+
+		double quantiteTotale(const string& nomProduit ) const{
+			return getQuantity()*getProduit().quantiteTotale(nomProduit);
+		}
+};
+
+class Recette{
+	private:
+		vector<Ingredient> listIn;
+		double nbFois_;
+		string nom_;
+	
+	public:
+		Recette(string n, double t = 1 )
+			:nom_(n), nbFois_(t){}
+
+		void ajouter( const Produit& p, double q){
+			listIn.push_back(  Ingredient(p, q*nbFois_) );
+		}
+
+		Recette adapter(double n){
+			Recette  r( nom_, nbFois_*n  );
+			for(auto it: listIn ) r.listIn.push_back( Ingredient(it.getProduit(), it.getQuantity()*n  ) );
+
+			return r;
+		}	
+
+		string toString( ) const{
+			string temp;
+			std::ostringstream os;			
+
+			os << nbFois_;
+
+			temp = " Recette \"" + nom_ + "\" x " + os.str() ;
+
+			double t=1; 
+			
+			for(auto it: listIn) 
+			{
+				std::ostringstream os;			
+				os << t;
+				temp +=  "\n " + os.str() + ". " + it.descriptionAdaptee();
+//				cout << it.descriptionAdaptee();
+				t+=1;
+			}
+
+			return temp;
+		}		
+		
+		double quantiteTotale(const string& nomProduit) const  {
+			double sum = 0;
+
+			for (auto it: listIn) sum+= it.quantiteTotale(nomProduit);
+
+			return sum;
+		}
+
+};
+
+class ProduitCuisine: public Produit{
+	private: 
+		Recette recette;
+
+	public: 
+		ProduitCuisine(string n, string t = "portion(s)")
+			:Produit(n,t),recette(n){}
+
+		void ajouterARecette(const Produit& produit, double quantity ){
+			recette.ajouter(produit, quantity);
+		}
+
+		const ProduitCuisine* adapter(double n) const override{
+			ProduitCuisine*	p = new ProduitCuisine(getNom());
+			p->recette.adapter(n);
+			return p;
+		}
+
+		const Recette& getRecette() const{ return recette; }
+
+		string  toString( ) const override {
+		//	cout<< getNom() << "\n"  << recette.toString() ;
+			return getNom()+"\n"+recette.toString();
+		}
+		
+		double quantiteTotale( const string& nomProduit  ) const override{
+			return nomProduit==getNom()?1:getRecette().quantiteTotale( nomProduit);
+		}
+
+};
+
 
 /*******************************************
  * Ne rien modifier après cette ligne.
