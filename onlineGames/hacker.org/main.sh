55732b6186be8ddce658fc534c10d0843400e323
Daniel Paredes <daleonpz@gmail.com>
2017-05-14 11:30:49 -0500

    update files

--- onlineGames/hacker.org/main.sh
+++ onlineGames/hacker.org/main.sh
@@ -13,3 +13,6 @@ cat lorem.txt | sed 's/[,.\r]//g' | tr ' ' '\n'  | sort | uniq -c | awk '$1==1'
 
 # Challenge 'I'm So Special' [Web]
 curl ifconfig.me
+
+# Challenge '3280' [Coding]
+cat rfc3280.txt | sed 's/[,.\r]//g' | tr ' ' '\n' | sort  | uniq -c | awk '{ print $1 "\t"  length($2) "\t" $2}' | awk '$2==9' | sort -k1 -n | tail -1 
