437a333533b0ae3ccb551de6621a9729ca8b9517
daleonpz <daleonpz@gmail.com>
2018-06-13 08:30:32 +0200

    add motor, bluetooth, button

--- C/arduino_follower/follower/follower.ino
+++ C/arduino_follower/follower/follower.ino
@@ -1,16 +1,17 @@
 #include <Servo.h>
- 
+#include <SoftwareSerial.h>
+
 ///////////////////////
 //  GLOBAL VARIABLES
 ///////////////////////
 Servo myservo;
+SoftwareSerial BT(5,6); // RX, TX
 
 ///////////////////////
 //  DEFINES
 ///////////////////////
 #define ANGLE_OFFSET 7
-#define SERVO_PORT 7
-// RED: power ,BROWN: GND, ORANGE: control
+#define SERVO_PORT 7    // RED: power, BROWN: GND, ORANGE: control
 
 #define L_trig  13
 #define L_echo  12
@@ -18,6 +19,17 @@ Servo myservo;
 #define M_echo  10
 #define R_trig  9
 #define R_echo  8 
+
+#define MOTOR_Pin 3       //Output pin number for the motor
+#define MOTOR_Max 120
+#define MOTOR_Min 0
+
+#define LED_BT A4
+#define LED_B A5
+#define Button 2
+
+int BTstate = 0;
+int motor_speed = 0;
 
 ///////////////////////
 //  FUNCTIONS
@@ -42,10 +54,61 @@ unsigned long readUltrasoundDistance(int trigPin, int echoPin){
     delayMicroseconds(10);// Sets the trigPin on HIGH state for 10 micro seconds
     digitalWrite(trigPin, LOW);
     duration = pulseIn(echoPin, HIGH);
-    distance = duration*0.01716; 
+    distance = duration*0.01716;
     Serial.print("\t");
     Serial.print(distance);
     return distance; 
+}
+
+void motor_ON() {
+    while (motor_speed < MOTOR_Max) {
+        motor_speed += 5;
+        analogWrite(MOTOR_Pin, motor_speed);
+        delay(20);
+  }
+}
+
+void motor_OFF() {
+    while (motor_speed > MOTOR_Min) {
+        motor_speed -= 5;
+        analogWrite(MOTOR_Pin, motor_speed);
+        delay(20);
+  }
+}
+
+void call_bart(){
+    int buttonState = digitalRead(Button);
+    if ( buttonState == HIGH ){
+        digitalWrite(LED_B, HIGH);
+        delay(1000);
+    }
+    else
+         digitalWrite(LED_B, LOW);
+}
+
+int validPassword() {
+    String pass = "amazon" ;
+    String S = "" ;
+    if (BT.available()) {
+        char c = BT.read(); 
+        while ( c != '\n') {           //Hasta que el caracter sea intro
+            S = S + c;
+            c = BT.read();
+            //delay(25) ;
+        }
+        Serial.print(S);
+    }
+    if (pass == S) return 1;
+    else return -1;
+}
+
+void authentication (){
+     if ( validPassword() > 0 ){
+        digitalWrite(LED_BT, HIGH);
+        delay(1000);
+     }
+     else 
+        digitalWrite(LED_BT, LOW);
 }
 
 void chooseDirection(){
@@ -68,23 +131,38 @@ void chooseDirection(){
     else go_straight();
 }
 
+
 ///////////////////////
 //  MAIN FUNCTIONS
 ///////////////////////
 void setup() {
-    myservo.attach(SERVO_PORT);  // Use pin 7 for the servo
-
+    myservo.attach(SERVO_PORT); 
+ 
     pinMode(L_trig, OUTPUT); 
     pinMode(L_echo, INPUT); 
     pinMode(M_trig, OUTPUT); 
     pinMode(M_echo, INPUT); 
     pinMode(R_trig, OUTPUT); 
-    pinMode(R_echo, INPUT); 
-    
-    Serial.begin(9600); // Starts the serial communication
+    pinMode(R_echo, INPUT);
+
+    pinMode(MOTOR_Pin, OUTPUT);     
+    digitalWrite(MOTOR_Pin, LOW);  
+ 
+    BT.begin(9600);
+    pinMode(LED_BT, OUTPUT);
+
+    pinMode(LED_B, OUTPUT);
+    pinMode(Button, INPUT);
+
+    Serial.begin(9600);
 }
 
 void loop() {
-    chooseDirection();
-    Serial.println("");
+    //chooseDirection();
+    //motor_ON();
+    //delay(3000);
+    //motor_OFF();
+    //delay(3000);
+    call_bart();
+    authentication();
 }
